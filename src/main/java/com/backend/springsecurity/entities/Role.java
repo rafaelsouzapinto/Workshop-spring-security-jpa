4a316db47b5a506d867b501dd859f83db9dd31d5
Antony Rafael Souza Pinto <154285174+rafaelsouzapinto@users.noreply.github.com>
2025-02-10 09:41:15 -0300

    inicio do projeto + entidades e ligações

---
+++ src/main/java/com/backend/springsecurity/entities/Role.java
@@ -0,0 +1,48 @@
+package com.backend.springsecurity.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name= "tb_roles")
+public class Role {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "role_id")
+	private Long roleId;
+	
+	private String name;
+
+	public Long getRoleId() {
+		return roleId;
+	}
+	public void setRoleId(Long roleId) {
+		this.roleId = roleId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public enum Values {
+		Basic(2L), 
+		Admin(1L);
+		
+		long roleId;
+		
+		Values(long roleId){
+			this.roleId = roleId;
+		}
+
+		public long getRoleId() {
+			return roleId;
+		}
+	}
+}
---
+++ src/main/java/com/backend/springsecurity/entities/User.java
@@ -0,0 +1,67 @@
+package com.backend.springsecurity.entities;
+
+import java.util.Set;
+import java.util.UUID;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_users")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	@Column(name = "user_id")
+	private UUID userId;
+	
+	@Column(unique = true)
+	private String username;
+	
+	private String password;
+	
+	// fetch é a forma como trazer os dados do DB
+	@ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	@JoinTable(
+			name = "tb_users_roles", 	//Nome da tabela de ligação entre users e roles
+			joinColumns = @JoinColumn(name = "users_id"), 	// nome da coluna que tera o id do user
+			inverseJoinColumns = @JoinColumn(name = "roles_id") 	// nome da coluna que tera o id de role
+	)
+	private Set<Role> roles;
+
+	public UUID getUserId() {
+		return userId;
+	}
+	public void setUserId(UUID userId) {
+		this.userId = userId;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Set<Role> getRoles() {
+		return roles;
+	}
+	public void setRoles(Set<Role> roles) {
+		this.roles = roles;
+	}
+	
+	
+}
